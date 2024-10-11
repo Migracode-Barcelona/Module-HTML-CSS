2e399a1f28959c5430e134a6ff179e0c3d7ade5e
Umair Riaz <umairriaz.ur2@gmail.com>
2024-10-11 02:52:18 +0200

    scroll to top button added

--- MultiPage-Clone/js/script.js
+++ MultiPage-Clone/js/script.js
@@ -5,3 +5,27 @@ document.querySelectorAll(".nav-menu-link").forEach((link) => {
     document.getElementById("menu-toggle").checked = false;
   });
 });
+
+// Scroll to top
+let hideButtonTimeout;
+
+window.onscroll = function() {
+    const button = document.getElementById("scrollToTopBtn");
+    const shopSection = document.getElementById("shop");
+    const shopSectionTop = shopSection.getBoundingClientRect().top;
+
+    if (shopSectionTop <= window.innerHeight) {
+        button.classList.add("visible"); // Add visible class to show button
+        // Clear the previous timeout and set a new one to hide the button after 2 seconds
+        clearTimeout(hideButtonTimeout);
+        hideButtonTimeout = setTimeout(() => {
+            button.classList.remove("visible"); // Remove visible class after 2 seconds
+        }, 2000);
+    } else {
+        button.classList.remove("visible"); // Remove visible class immediately
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
